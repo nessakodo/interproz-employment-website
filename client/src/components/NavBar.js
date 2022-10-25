3bb90998835230d809208fda7e5acc69a7e3614e
aspencass <caspenw1@gmail.com>
2022-10-25 10:37:37 -0600

    home save

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -116,8 +116,8 @@ function NavBar({loggedIn, setCurrentCandidate, setLoggedIn, setVisible} ) {
             <div>
                 <img src={homepage_pic} alt="Picture Courtesy of Tiger Lily on Pexels" />
                 <header class="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
-                <div class="p-20 text-2xl text-black bg-gray-300 bg-opacity-50 fixed h-100 w-20 inset-y-0 left-0">
-                    EXCEL PERSONNEL <br /> We specialize in Administration, Forklift, Packaging, Quality Control, Shipping & Receiving and Warehouse talent. <br />Begin your search 
+                <div class="p-20 text-2xl text-black bg-gray-300 bg-opacity-50">
+                    EXCEL PERSONNEL <br /> We specialize in Administration, Forklift, Packaging, Quality Control, Shipping & Receiving and Warehouse talent. <br />Begin your search <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
             </header>
             </div>
